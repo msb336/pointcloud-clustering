0b90aef6ec3a98db89af47889ff5baa9ce6aa5c7
Matthew Brown <mattbrown@n3-125-44.dhcp.drexel.edu>
2018-04-18 17:29:02 -0400

    added parameter text files
    and removed build folders / cmake files

--- matlab/read_off.m
+++ matlab/read_off.m
@@ -12,14 +12,13 @@ if strcmp(header, 'OFF')
         nface = fscanf(fid, '%d', 1);
         fscanf(fid, '%d', 1);
 else
-    dimension = str2num(header);
-    numpoints = fscanf(fid, '%d', 1);
+    error('incorrect file format');
 end
 
 vertex = dlmread(filename, ' ', [2 0 1+numpoints 2]);
 face = dlmread(filename, ' ', 2+numpoints, 0);
-s = size(face,2);
-face = face(:, (s - face(1,1)+1):s) + 1;
-
+face = face(all(floor(face)==face, 2), :);
+s = face(1);
+face = face(:, end-s+1:end) + 1;
 
 fclose(fid);
