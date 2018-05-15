9ece4de4293ee1169b798b312032983f5476fdc9
msb336 <msb336@drexel.edu>
2018-05-15 18:26:38 -0400

    working newton raphson regression for cross section cost minimization ! next step is to add boundaries, rotation, and iteration throughout the beam. This is cool.

--- cross-section-estimation/estimate-normals.cpp
+++ cross-section-estimation/estimate-normals.cpp
@@ -4,14 +4,19 @@ void newtonRaphson ( float &cost, Polygon &polygon, pointCloud &pointset , float
 {
   pointCloud temp = pointset;
   float del = 10000;
-  while ( del > tolerance )
+  float prevdel = 0;
+
+  while ( (del > tolerance) )
   {
     float oldcost = cost;
     costfunction ( cost, polygon, pointset, temp);
+    std::cout << "Move cost: " << cost << std::endl;
+
+    prevdel = del;
     del = polygon.rewrite ( cost, oldcost );
+    std::cout << del << " " << tolerance << std::endl;
   }
   pointset = temp;
-
 }
 
 int main ( int argc, char** argv)
@@ -22,24 +27,28 @@ int main ( int argc, char** argv)
   std::string fullfile    	=   parameters[0];
   // float radius 			  	=	stof ( parameters[1] );
   float tolerance			=	stof ( parameters[1] );
+  float del_tolerance = stof ( parameters[2] );
   int 	minclust 			= 	1;
   pointCloud::Ptr cloud 	= 	loadcloud ( fullfile );
-  pointCloud::Ptr newcloud ( new pointCloud );
+  pointCloud::Ptr newcloud ( new pointCloud ), tempcloud ( new pointCloud );
   newcloud = cloud;
   ////////////// Build Initial Beam Cross-Section //////////////////////
   Polygon poly (5, 3, 2, 1, Eigen::Vector3f (0,0,0) );
 
   float cost = 0;
-
   ////////// Determining Cost of fitting points to beam ///////////
   /*    Our goal here to is to find the optimal beam cross-section parameters that minimize 
         the amount of distance traversed by the points within range of the cross-section plane
   */ 
   visualize ( cloud );
   std::cout << "Moving points to polygon and calculating cost " << std::endl;
-  newtonRaphson ( cost, poly, *cloud, 5 );
+    pointsnearplane ( poly, *cloud, *tempcloud, tolerance);
+    std::cout << "cloud size: " << cloud->points.size() << " temp cloud size: " << tempcloud->points.size() <<std::endl;
+    newtonRaphson ( cost, poly, *tempcloud, del_tolerance );
+    *newcloud += *tempcloud;
 
-  visualize ( cloud );
+
+  visualize ( tempcloud );
 
 
   return 0;
