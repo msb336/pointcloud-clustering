ebdacc5670155ef0b960678fcccd04373e1c45b7
msb336 <msb336@drexel.edu>
2018-05-16 11:40:45 -0400

    works for 2d case, next step is to have it iterate through 3d cloud

--- cross-section-estimation/estimate-normals.cpp
+++ cross-section-estimation/estimate-normals.cpp
@@ -13,11 +13,24 @@ void newtonRaphson ( float &cost, Polygon &polygon, pointCloud &pointset , float
     std::cout << "Move cost: " << cost << std::endl;
 
     prevdel = del;
+    if ( cost == oldcost )
+    { break; }
     del = polygon.rewrite ( cost, oldcost );
-    float oldcost = cost;
+    oldcost = cost;
     std::cout << del << " " << prevdel << " "  << tolerance << std::endl;
   }
+
   pointset = temp;
+  std::cout << "Final values:\n"
+    << "thickness: "<< polygon.thickness  << "\n"
+    << "width: "    << polygon.width      << "\n"
+    << "height 1: " << polygon.height1    << "\n"
+    << "height 2: " << polygon.height2    << "\n"
+    << "theta: "    << polygon.theta      << "\n"
+    << "phi: "      << polygon.phi        << "\n"
+    <<"del norm: "  << del << std::endl;
+    polygon.makeCloud();
+
 }
 
 int main ( int argc, char** argv)
@@ -32,7 +45,7 @@ int main ( int argc, char** argv)
   int 	minclust 			= 	1;
   pointCloud::Ptr cloud 	= 	loadcloud ( fullfile );
   pointCloud::Ptr newcloud ( new pointCloud ), tempcloud ( new pointCloud );
-  newcloud = cloud;
+
   ////////////// Build Initial Beam Cross-Section //////////////////////
   Polygon poly (5, 3, 2, 1, Eigen::Vector3f (0,0,0) );
 
@@ -41,15 +54,17 @@ int main ( int argc, char** argv)
   /*    Our goal here to is to find the optimal beam cross-section parameters that minimize 
         the amount of distance traversed by the points within range of the cross-section plane
   */ 
-  // visualize ( cloud );
+  visualize ( cloud );
   std::cout << "Moving points to polygon and calculating cost " << std::endl;
     pointsnearplane ( poly, *cloud, *tempcloud, tolerance);
     std::cout << "cloud size: " << cloud->points.size() << " temp cloud size: " << tempcloud->points.size() <<std::endl;
     newtonRaphson ( cost, poly, *tempcloud, del_tolerance );
     *newcloud += *tempcloud;
-
-
-  visualize ( tempcloud );
+  pointCloud::Ptr vertex ( new pointCloud );
+  *vertex += poly.vertices;
+  std::vector<pointCloud::Ptr> set;
+  set.push_back(newcloud); set.push_back(vertex);
+  visualize ( set );
 
 
   return 0;
