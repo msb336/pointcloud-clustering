16c872621c13cfaccfe13c9ce4aade8ae9fc9bd0
Matthew Brown <mattbrown@Matthews-MBP.fios-router.home>
2018-05-31 09:11:01 -0400

    no animation

--- stuff2takehome/hdl32epointcloudmaker.m
+++ stuff2takehome/hdl32epointcloudmaker.m
@@ -1,9 +1,6 @@
 clearvars;clc
 laserpos = linspace(-10.67*pi/180,30.67*pi/180,32)';
-location = @(x)[2*x-15 sin(3*x)+5 5];
-% [x y] = meshgrid(-1:0.01:1, -1:0.01:1);
-% pts = [x(:) 5*ones(size(x(:))) y(:)];
-% objects.boundaries = pts;
+location = @(x)[4*x-15 2*sin(3*x)+5 5];
 
 objects.boundaries = unique(dlmread('simulated-beam-0noise (1).xyz'), 'rows');
 rotation_rate = 20;
@@ -24,58 +21,68 @@ pcshow(objects.boundaries);
 xlabel('x');ylabel('y');zlabel('z');view([125 25]);axis([-15 15 -15 15 -5 45]);
 subplot(1,2,2);
 im = [];
+path = [];
 k = 1;
 gif_file='test.gif';
-for time = 0:(5/scan_per_rotation):10
+
+duration = 7.5;
+
+for time = 0:(1/scan_per_rotation):duration
     
-    hold off;
+    %     hold off;
     plot3(0,0,0);
     loc = location(time);
-
+    path = [path; loc];
     rot_angle = time*rotation_rate*2*pi;
     vectors = [ cos(laserpos)*cos(rot_angle) ...
-            cos(laserpos)*sin(rot_angle) ...
-            sin(laserpos) ];
-    hold on; 
-     for i = 1:32
-         point = vectors(i,:);
-          plot3(loc(1)+[0 point(1)], loc(2)+[0 point(2)], loc(3)+[0 point(3)], '-');
-     end
-    view([125 25])
-    xlabel('x');ylabel('y');zlabel('z');
+        cos(laserpos)*sin(rot_angle) ...
+        sin(laserpos) ];
+    %     hold on;
+    
+    %     plot3(path(:,1), path(:,2), path(:,3), 'r-');
+    %      for i = 1:32
+    %          point = vectors(i,:);
+    %           plot3(loc(1)+[0 point(1)], loc(2)+[0 point(2)], loc(3)+[0 point(3)], '-');
+    %      end
+    %     view([125 25])
+    %     xlabel('x');ylabel('y');zlabel('z');
+    %     pause(0.000001)
     
     [r, xyzclean] = collisiondetect(objects, vectors, loc);
-
+    
     xyz = [ r.*cos(laserpos).*cos(rot_angle) ...
-            r.*cos(laserpos).*sin(rot_angle) ...
-            r.*sin(laserpos) ];
-        
+        r.*cos(laserpos).*sin(rot_angle) ...
+        r.*sin(laserpos) ];
+    
     p = [p; xyz(any(xyz~=0, 2),:)+loc];
     cleanpts = [cleanpts; xyzclean];
-    plot3(cleanpts(:,1), cleanpts(:,2), cleanpts(:,3), 'b.');
-    axis([-15 15 -15 15 -5 45]);
+    %     plot3(cleanpts(:,1), cleanpts(:,2), cleanpts(:,3), 'b.');
+    
+    %     axis([-15 15 -15 15 -1 12]);
     
     
     
-    f = getframe(gcf());
-    im = frame2im(f);
+    %     f = getframe(gcf());
+    %     im = frame2im(f);
+    %
+    %     [A,map] = rgb2ind(im, 256);
+    %     if k == 1
+    %         imwrite(A,map,gif_file,'gif','LoopCount',Inf,'DelayTime',0);
+    %     else
+    %         imwrite(A,map,gif_file, 'gif', 'WriteMode', 'append', 'DelayTime', 0);
+    %     end
+    %     k=k+1;
     
-    [A,map] = rgb2ind(im, 256);
-    if k == 1
-        imwrite(A,map,gif_file,'gif','LoopCount',Inf,'DelayTime',0);
-    else
-        imwrite(A,map,gif_file, 'gif', 'WriteMode', 'append', 'DelayTime', 0);
-    end
-    k=k+1;
-    
-    
-    
-    if (time/10) >= nextboy
+    if (time/duration) >= nextboy
         nextboy
         nextboy = nextboy+0.25;
     end
-        
+    
 end
 
-plot3(p(:,1), p(:,2), p(:,3), 'r.')
-plot3(cleanpts(:,1), cleanpts(:,2), cleanpts(:,3), 'b.')
+
+%%
+
+lidarcloud = pointCloud(p);
+close;pcshow(lidarcloud);hold on; plot3(path(:,1), path(:,2), path(:,3), 'r-');axis([-15 15 -15 15 -1 12]);pbaspect([1 1 1])
+% plot3(cleanpts(:,1), cleanpts(:,2), cleanpts(:,3), 'b.')
